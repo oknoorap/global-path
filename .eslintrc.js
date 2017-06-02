8587052b81953b8b5f7c0a26bfa5c44657cb9731
oknoorap <oknoorap@gmail.com>
2017-06-02 08:59:47 +0700

    fix: indentation

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,20 +9,20 @@ module.exports = {
   },
   "rules": {
     "indent": [
-    "error",
-    2
+      "error",
+      2
     ],
     "linebreak-style": [
-    "error",
-    "unix"
+      "error",
+      "unix"
     ],
     "quotes": [
-    "error",
-    "single"
+      "error",
+      "single"
     ],
     "semi": [
-    "error",
-    "never"
+      "error",
+      "never"
     ]
   }
 };
